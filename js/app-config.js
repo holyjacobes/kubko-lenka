27109a79eeccfece2d2fa86539457a862d97488d
Jakub Smolnický <jakub.smolnicky@student.tuke.sk>
2025-08-18 23:39:06 +0200

    Making global config, adding Digital message

---
+++ js/app-config.js
@@ -0,0 +1,6 @@
+// js/app-config.js
+export const SUPABASE_URL = "https://byowsxlaoljqrbllbiad.supabase.co";
+export const ANON_KEY     = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5b3dzeGxhb2xqcXJibGxiaWFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwMzcxNzAsImV4cCI6MjA2ODYxMzE3MH0.dOBoxQqiMBYT5PIbwQUEq-rVBIO5pp1IjG89EO04LD4";
+
+export const BUCKET       = "kubko-lenka";
+export const EXCHANGE_FN  = "exchange-token"; // pre konzistenciu
